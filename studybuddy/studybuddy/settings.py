8f6f7dbc3b86020420f46271fb6e567ec16b51b5
rahulbabu7 <rahulbabu436@gmail.com>
2024-06-10 12:00:28 +0530

    getting data correctly from env now

--- studybuddy/studybuddy/settings.py
+++ studybuddy/studybuddy/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'studybuddy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'StudyBuddy',
+        'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
